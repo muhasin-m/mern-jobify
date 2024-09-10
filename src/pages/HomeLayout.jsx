242ffd3ba43c02b8c9c1477093e934e35339265f
muhasin <mahmudmuhasin@gmail.com>
2024-09-10 13:33:59 +0200

    add landing page

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -2,10 +2,9 @@ import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
   return (
-    <div>
-      <nav>navabr</nav>
+    <>
       <Outlet />
-    </div>
+    </>
   );
 };
 export default HomeLayout;
