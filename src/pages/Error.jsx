99bbdd9287c4b13d9481813c0d830868fe08cc8b
muhasin <mahmudmuhasin@gmail.com>
2024-09-12 12:09:25 +0200

    add register page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,14 +1,26 @@
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/images/not-found.svg";
 
 const Error = () => {
   const error = useRouteError();
-  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not foujnd" />
+          <h3>Ohh! page not found</h3>
+          <p>We can't seem to find the page you are looking for</p>
+          <Link to={"/dashboard"}>back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
 
   return (
-    <div>
-      <h1>Error Page</h1>
-      <Link to={"/"}>back home</Link>
-    </div>
+    <Wrapper>
+      <h3>Something went wrong!</h3>
+    </Wrapper>
   );
 };
 export default Error;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,34 @@
 import { Link } from "react-router-dom";
-
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { FormRow, Logo } from "../components";
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to="/login">Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" defaultValue="John" />
+        <FormRow
+          type="text"
+          name="lastName"
+          labelText="last name"
+          defaultValue="Doe"
+        />
+        <FormRow type="text" name="location" defaultValue="earth" />
+        <FormRow type="email" name="email" defaultValue="john@gmail.com" />
+        <FormRow type="password" name="password" defaultValue="secretss123" />
+
+        <button type="submit" className="btn btn-block">
+          Submit
+        </button>
+        <p>
+          Already a member
+          <Link to={"/login"} className="member-btn">
+            Login
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default Register;
